d2943dd2efc79106c6c9ed3ee1a3d8572efd9d61
jcl <chaelynjang@lotte.net>
2024-12-19 13:23:03 +0900

    volatile 사용하여 다중 스레드에서 특정 변수의 값을 메모리에 직접 접근하여 읽을 수 있도록 추가

--- src/main/java/com/example/javaadv1/thread/volatile1/VolatileFlagMain.java
+++ src/main/java/com/example/javaadv1/thread/volatile1/VolatileFlagMain.java
@@ -18,15 +18,18 @@ public class VolatileFlagMain {
     }
 
     static class MyTask implements Runnable {
-        boolean runFlag = true;
-        // volatile boolean runFlag = true;
+        //boolean runFlag = true;
+        // 캐시 메모리가 아닌 메인 메모리에 직접 접근하여 값을 read
+        volatile boolean runFlag = true;
         @Override
         public void run() {
             log("task 시작");
             while(runFlag) {
                 // runFlag가 false 되면 종료
+                // 만약 여기서 뭔가 sout 혹은 log과 같은 동작을 하면 컨텍스트 스위칭이 일어나서 volatile을 사용안해도 제대로 반영될 수는 있음
+                // -> but, 100% 보장되는 것은 아니므로 volatile 사용 권장
             }
-            // runFlag가 false가 되어도 무한 반복되어 해당 로그 안찍힘
+            // volatile 생략하면 main 에서 runFlag가 false가 되어도 무한 반복되어 해당 로그 안찍힘
             log("task 종료");
         }
     }
